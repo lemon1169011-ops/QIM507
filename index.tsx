1a83c767c5c18d1229f37d165f02db54104c1c5e
lemon1169011-ops <lemon1169011@gmail.com>
2026-01-06 15:27:52 +0800

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
-
-import React from 'react';import './index.css'
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App.tsx';
+import './index.css'; // <--- 关键！这一行必须有，且建议单独一行
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
