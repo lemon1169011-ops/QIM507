10fda3760bb7f26c277f361e7369647ddb7ace69
lemon1169011-ops <lemon1169011@gmail.com>
2026-01-06 15:28:39 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,15 @@
 export default {
   content: [
     "./index.html",
-    "./*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}"
+    "./src/**/*.{js,ts,jsx,tsx}", // 扫描 src 文件夹下的所有代码
+    "./*.{js,ts,jsx,tsx}"         // 扫描根目录下的代码 (比如 App.tsx)
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'pulse-slow': 'pulse 3s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      },
+    },
   },
   plugins: [],
 }
